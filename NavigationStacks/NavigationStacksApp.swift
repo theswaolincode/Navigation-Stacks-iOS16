cf91fa219e604eb0bb1eb1e703a5daa1c6d60322
Daniel Ayala <imediayala@icloud.com>
2022-11-10 18:02:02 +0100

    Initial Commit

---
+++ NavigationStacks/NavigationStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksApp.swift
+//  NavigationStacks
+//
+//  Created by Daniel Ayala on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
